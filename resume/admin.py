8a10bea1a73c6e2b8fe79115605f185ba67a1ff0
FREDERICO23 <adhingafredrick@gmail.com>
2023-11-27 12:12:06 +0300

    update ui and regsiter models

--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import GeneratedCoverLetter, GeneratedResume
 
 # Register your models here.
+admin.site.register(GeneratedCoverLetter)
+admin.site.register(GeneratedResume)
