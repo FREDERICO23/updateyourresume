3ea7850c2cc35cf92b3fca4e033ce1e47fd995c6
FREDERICO23 <adhingafredrick@gmail.com>
2024-02-18 19:53:46 +0300

    Update dependencies and comment out debug toolbar

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "crispy_forms",
     "crispy_bootstrap5",
-    "debug_toolbar",
+    # "debug_toolbar",
     "easyaudit",
     # Local
     "accounts",
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",  # WhiteNoise
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",  # Django Debug Toolbar
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",  # Django Debug Toolbar
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
